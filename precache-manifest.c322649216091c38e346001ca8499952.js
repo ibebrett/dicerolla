f9d965fcfe4a03a2cf9f5661a74808983719265b
Brett Jurman <i.be.brett@gmail.com>
2019-03-24 17:27:36 -0400

    Updates

---
+++ precache-manifest.c322649216091c38e346001ca8499952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24188748cb949f185d54",
+    "url": "/dicerolla/static/js/runtime~main.1e980df0.js"
+  },
+  {
+    "revision": "ee956b29c42054c07ba5",
+    "url": "/dicerolla/static/js/main.f834b4f2.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/dicerolla/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "ee956b29c42054c07ba5",
+    "url": "/dicerolla/static/css/main.7f160453.chunk.css"
+  },
+  {
+    "revision": "ac49b1a858a4f50003597dad29762396",
+    "url": "/dicerolla/index.html"
+  }
+];
